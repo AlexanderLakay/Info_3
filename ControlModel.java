05167ec9c15897b9a47fabd778c495069c62c915
AlexanderLakay <a.lakay@ok.de>
2017-05-26 10:21:09 +0200

    JavaDoc erstellt

--- ControlModel.java
+++ ControlModel.java
@@ -1,36 +1,41 @@
 import hsrt.mec.controldeveloper.io.IOType;
 import hsrt.mec.controldeveloper.io.TextFile;
-
 import java.io.File;
 import java.util.Vector;
 
 
+/**
+ * 
+ * @author Alexander Lakay
+ * Klasse zum Verwalten der Befehlsliste
+ *
+ */
 public class ControlModel {
 
 	private static ControlModel instance = null;
 	private CommandType commandTypes[] = new CommandType[4];
 	private CommandList controlProcess = new CommandList();
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
 	
 	private ControlModel()
 	{
 		
 	}
 	
+	/**
+	 * Gibt das Objekt des ControlModels zurueck und erzeugt gegebenenfalls ein neues
+	 * @return ControlModel
+	 */
 	public static ControlModel getInstance()
 	{
-		
-		if (instance==null)
-			instance= new ControlModel();
+		// Esgibt immer nur eine Instanz der Klasse
+		if (instance == null)
+			instance = new ControlModel();
 		return instance;
 	}
 	
+	/**
+	 * Befuellt die Liste mit verfuegbaren Befehlen
+	 */
 	public void createCommandTyes()
 	{
 		commandTypes[0] = new CommandType("Direction");
@@ -41,32 +46,63 @@ public class ControlModel {
 	}
 	
 	// Zugriff auf die verkettete Liste
+	/**
+	 * Fuegt ein neues Element zur Liste hinzu
+	 * @param c
+	 * @return Gibt true zurueck, wenn das Element hinzugefuegt wurde
+	 */
 	public boolean add(Command c) 
 	{
 		return controlProcess.add(c);
 	}
 	
+	/**
+	 * Entfernt das angegebene Element aus der Liste
+	 * @param pos Index des zu entfernenden Elements
+	 * @return Gibt true zurueck, wenn das Element entfernt wurde
+	 */
 	public boolean remove(int pos)
 	{
 		return controlProcess.remove(pos);
 	}
 	
+	/**
+	 * Gibt das Command Objekt an der stelle pos in der Liste zurueck
+	 * @param pos Index des Listenelements
+	 * @return
+	 */
 	public Command get(int pos)
 	{
 		return controlProcess.get(pos);
 	}
 	
+	/**
+	 * Schiebt das ausgewaehlte Element um eine Position in der Liste nach oben
+	 * @param pos Index des zu verschiebenden Elements
+	 * @return Gibt true zurueck, wenn das Element verschoben wurde
+	 */
 	public boolean moveUp(int pos)
 	{
 		return controlProcess.moveUp(pos);
 	}
 	
+	/**
+	 * Schiebt das ausgewaehlte Element um eine Position in der Liste nach unten
+	 * @param pos Index des zu verschiebenden Elements
+	 * @return Gibt true zurueck, wenn das Element verschoben wurde
+	 */
 	public boolean moveDown(int pos)
 	{
 		return controlProcess.moveDown(pos);
 	}
 	
 	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	/**
+	 * Laedt die Befehlsliste aus der angegebenen Datei
+	 * @param f Datei, die geladen werden soll
+	 * @return Gibt true zutueck, wenn die Datei geladen wurde
+	 */
 	public boolean load(File f)
 	{
 		controlProcess = new CommandList();
@@ -78,7 +114,7 @@ public class ControlModel {
 		
 		int i = 0;
 		while(true){
-			
+			// Erzeugt anhand der Namen entsprechende Command Objekte und haengt sie an die Befehlsliste an
 			if(daten.get(i).toString().equals("Direction")){
 				c = new Direction("Direction", Integer.parseInt((daten.get(i+1).toString())));
 				i+=2;
@@ -94,6 +130,7 @@ public class ControlModel {
 			}else if(daten.get(i).toString().equals("Pause")){
 				c = new Pause("Pause", Double.parseDouble(daten.get(i+1).toString()));
 				i += 2;
+			// "Ende" Markiert das Listenende
 			}else if(daten.get(i).toString().equals("Ende")){
 					break;
 			}
@@ -104,8 +141,12 @@ public class ControlModel {
 		return antwort;
 	}
 	
-	
-	//@SuppressWarnings("unchecked")
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	/**
+	 * Speichert die Befehlsliste in die angegebene Datei
+	 * @param f Datei, in die die Befehlsliste gespeichert werden soll
+	 * @return gibt true zurueck, wenn die Datei gespeichert wurde
+	 */
 	public boolean save(File f)
 	{
 		IOType ausgabe = new TextFile(f, false);
@@ -115,11 +156,12 @@ public class ControlModel {
 		int i = 0;
 		
 		while(true){
+			// Durchlaeuft die Befehlsliste und schreibt die Parameter in den Vector
 			Command c = controlProcess.get(i++);
+			
+			// Ende der Befehlsliste
 			if(c == null)
 				break;
-			//@SuppressWarnings("rawtypes")
-			
 			
 			if(c.getName() == "Direction"){
 				daten.add(c.getName());
@@ -146,6 +188,7 @@ public class ControlModel {
 			
 			
 		}
+		// "Ende" Markiert das Listenende
 		daten.add("Ende");
 		antwort = ausgabe.write(daten);
 		ausgabe.close();
@@ -158,6 +201,10 @@ public class ControlModel {
 		
 	}
 	
+	/**
+	 * Gibt die CommandList Instanz der Befehlsliste zurueck
+	 * @return Gibt die CommandList Instanz der Befehlsliste zurueck
+	 */
 	public CommandList getControllProcess()
 	{
 		return controlProcess;
