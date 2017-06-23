ce3e3ba63c791f1404bce1956c293635177e263d
AlexanderLakay <a.lakay@ok.de>
2017-06-23 13:03:46 +0200

    Add files via upload

--- GUI_ausgabe.java
+++ GUI_ausgabe.java
@@ -7,12 +7,21 @@ import javax.swing.JTextArea;
 
 import hsrt.mec.controldeveloper.core.com.command.ICommand;
 
+/**
+ * 
+ * @author Alexander Lakay
+ * Klasse zur Ausgabe der ausgefuehrten Befehle
+ *
+ */
 public class GUI_ausgabe extends JPanel{
 	private static GUI_ausgabe ausgabe = null;
 	private JTextArea tA = new JTextArea();
 	//private JPanel panelAusgabe = new JPanel();
 	
-
+/**
+ * 
+ * @return Gibt die einzige Instanz der Klasse zurueck
+ */
 public static GUI_ausgabe getInstance()
 {
 	if(ausgabe == null)
@@ -29,11 +38,18 @@ private GUI_ausgabe()
 	
 }
 
+/**
+ * Wird vom Controlmodell aufgerufen
+ * @param c Ausgefuehrtes Command
+ */
 public void commandPerformed(ICommand c)
 {
 	tA.append(c.toString() + "\n");
 }
 
+/**
+ * Leert das ausgabefeld
+ */
 public void clear()
 {
 	tA.setText("");
